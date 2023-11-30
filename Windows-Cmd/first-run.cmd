8ee8e993f77258146723b327d818fd6c0207cdbc
若瞳 <142956663+rotomoe@users.noreply.github.com>
2023-11-30 22:28:23 +0800

    Update and rename api.cmd to first-run.cmd

--- Windows-Cmd/first-run.cmd
+++ Windows-Cmd/first-run.cmd
@@ -4,3 +4,5 @@ set zoneid=ca99822ef99dd87814923f464e764486 (c t u zoneid)
 set domain=first-user.public.ddns.rotomoe (c t u domain-name)
 set ip=%1
 curl -X POST https://api.cloudflare.com/client/v4/zones/%zoneid%/dns_records -H "Content-Type: application/json" -H "X-Auth-Email: %account%" -H "X-Auth-Key: %apikey%" -d"{"content": "%ip%", "name": "%domain#", "proxied": false, "type": "A"}"
+echo Copy the record-id! Next, close this program! (You can delete it)
+pause
